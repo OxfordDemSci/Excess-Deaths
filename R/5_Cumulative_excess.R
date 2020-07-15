7be30d6caf69c5ab1fef1ecbd873eb9cc0e410d8
Jonas Schöley <jschoeley@gmail.com>
2020-07-15 12:10:18 +0200

    Add PIs to Figures 1 and 2

--- R/5_Cumulative_excess.R
+++ R/5_Cumulative_excess.R
@@ -2,91 +2,144 @@
 
 rm(list=ls())
 
-library(data.table)
-library(ggplot2)
-library(scales)
+library(tidyverse)
 library(patchwork)
 
-source('R/4_Excess_deaths.R')
+fig <- list()
 
+# Constants -------------------------------------------------------
 
-Total.deaths.EW <- results.2020[age.n != 0 ,list(excess.dx = sum(excess.dx)), by = .(year,sex,model,week,date)]
+cnst <- list()
 
-Both.sexes     <- Total.deaths.EW[, list(excess.dx = sum(excess.dx), sex = 'both'), by = .(year,model,week,date)]
+cnst$age.labels <-
+  c(
+    `15 to 44` = 15,
+    `45 to 64` = 45,
+    `65 to 74` = 65,
+    `75 to 85` = 75,
+    `85+` = 85
+  )
+cnst$color.sex <-
+  c(`f` = '#72B2B4', `m` = '#B4A097')
 
-Total.deaths.EW <- rbind(Total.deaths.EW,Both.sexes[,c('year','sex','model','week','date','excess.dx')])
+source('R/Figure_specifications.R')
 
-Total.deaths.EW[, cum.excess.deaths := cumsum(excess.dx), by = .(year,sex,model)]
+# Load data -------------------------------------------------------
 
-# total population plot
-fig1.data <- Total.deaths.EW[sex!= 'both' & model == 'gam.final' & week >= w1]
+load('Data/Excess_Deaths.RData')
 
-lab.fig <- paste0(c('Females (','Males ('),
-                  prettyNum(round(fig1.data[,sum(excess.dx), by = .(sex)]$V1),big.mark=",",scientific=FALSE),')')
+# Plot cumulative excess deaths by sex ----------------------------
 
-Figure.1 <- ggplot(fig1.data)+
-  geom_hline(yintercept = 0, color = "black", size = 1)+
-  geom_path(aes(date, cum.excess.deaths, color = sex), size = 1.2,show.legend = F, lineend = "round")+
-  #scale_color_manual('Sex' , values = c("#72B2B4", "#B4A097",'#615652'),labels = c('Females','Males','Both'))+
-  scale_color_manual('Sex' , values = c("#72B2B4", "#B4A097"),labels = c('Females','Males'))+
-  scale_y_continuous(label=comma,breaks = seq(0,50000,10000))+
-  coord_cartesian(xlim =as.Date(c("2020-03-09", "2020-06-29")))+
-  theme_gray(base_size = 16)+
-  theme(
-    aspect.ratio = 1,
-    text = element_text(face = 1)
-  )+
+fig$figure1 <-
+  results$excess.deaths.week.sex.age %>%
+  filter(model == 'gam.poisson' & age.n != 0) %>%
+  mutate(age.n = factor(age.n, cnst$age.labels, names(cnst$age.labels))) %>%
+  group_by(date, sex) %>%
+  summarise_at(
+    vars(cum.excess.deaths.qlo, cum.excess.deaths.qhi, cum.excess.deaths.avg),
+    sum
+  ) %>%
+  ggplot() +
+  geom_ribbon(
+    aes(
+      x = date,
+      ymin = cum.excess.deaths.qlo,
+      ymax = cum.excess.deaths.qhi,
+      fill = sex
+    ),
+    alpha = 0.2
+  ) +
+  geom_line(
+    aes(x = date, y = cum.excess.deaths.avg, color = sex)
+  ) +
+  geom_point(
+    aes(x = date, y = cum.excess.deaths.avg, color = sex),
+    size = 1
+  ) +
+  annotate(
+    'text',
+    x = as.Date('2020-07-01'), y = 28e3,
+    label = 'Male', color = cnst$color.sex['m']
+  ) +
+  annotate(
+    'text',
+    x = as.Date('2020-07-01'), y = 24e3,
+    label = 'Female', color = cnst$color.sex['f']
+  ) +
+  scale_x_date(limits = as.Date(c('2020-03-01', '2020-07-07'))) +
+  scale_y_continuous(labels = scales::label_comma()) +
+  scale_color_manual(values = cnst$color.sex) +
+  scale_fill_manual(values = cnst$color.sex) +
   labs(
     x = NULL,
-    y = "Excess deaths"
-  )+
-  annotate("text", x = as.Date("2020-06-30"), y = (round(fig1.data[,sum(excess.dx), by = .(sex)]$V1) + c(-1500,+2500)),
-           label = lab.fig,
-           #color = c("#72B2B4", "#B4A097",'#615652'),
-           color = c( "#72B2B4", "#B4A097"),
-           size = 6.5, hjust = 1, vjust = 1)
-Figure.1
-pdf(file = 'Figures/Figure_1.pdf',width = 7 ,height = 9)
-Figure.1
-dev.off()
+    y = 'Cumulative excess deaths'
+  ) +
+  fig_spec$MyGGplotTheme(hgrid = TRUE, scaler = 1.3, show_legend = FALSE)
 
+fig_spec$ExportPDF(
+  fig$figure1, filename = 'Figure_1', path = 'Figures',
+  width = fig_spec$width, height = 0.6*fig_spec$width
+)
 
-################################################################################
+# Plot cumulative excess by sex and age ---------------------------
 
-Both.sexes.age      <- results.2020[, list(excess.dx = sum(excess.dx), sex = 'both'), by = .(year,model,week,age.n.fct,date)]
-
-Total.deaths.EW.age <- rbind(results.2020[,c('year','sex','model','week','date','age.n.fct' ,'excess.dx')],
-                             Both.sexes.age[,c('year','sex','model','week','date','age.n.fct' ,'excess.dx')])
-
-Total.deaths.EW.age[, cum.excess.deaths := cumsum(excess.dx), by = .(year,sex,age.n.fct,model)]
-
-fig2.data <- Total.deaths.EW.age[sex!= 'both' & model == 'gam.final' & age.n.fct != '0 to 14']
-
-Figure.2 <- ggplot(fig2.data)+
-  geom_hline(yintercept = 0, color = "black", size = 1)+
-  geom_path(aes(date, cum.excess.deaths, color = sex), size = 1,show.legend = T, lineend = "round")+
-  #scale_color_manual('Sex' , values = c("#72B2B4", "#B4A097",'#615652'),labels = c('Females','Males','Both'))+
-  scale_color_manual('Sex' , values = c("#72B2B4", "#B4A097"),labels = c('Females','Males'))+
-  theme_gray(base_size = 16)+
-  scale_y_continuous(label=comma)+
-  coord_cartesian(xlim =as.Date(c("2020-03-09", "2020-06-29")))+
-  theme(
-    aspect.ratio = 1,
-    text = element_text(face = 1),
-    legend.position = c(.85,.25)
-  )+
+fig$figure2 <-
+  results$excess.deaths.week.sex.age %>%
+  filter(model == 'gam.poisson' & age.n != 0) %>%
+  mutate(age.n = factor(age.n, cnst$age.labels, names(cnst$age.labels))) %>%
+  ggplot() +
+  geom_ribbon(
+    aes(
+      x = date,
+      ymin = cum.excess.deaths.qlo,
+      ymax = cum.excess.deaths.qhi,
+      fill = sex
+    ),
+    alpha = 0.2
+  ) +
+  geom_line(
+    aes(x = date, y = cum.excess.deaths.avg, color = sex)
+  ) +
+  geom_point(
+    aes(x = date, y = cum.excess.deaths.avg, color = sex),
+    size = 1
+  ) +
+  facet_wrap(~age.n) +
+  scale_x_date(limits = as.Date(c('2020-03-01', '2020-07-07'))) +
+  scale_y_continuous(labels = scales::label_comma()) +
+  scale_color_manual(values = cnst$color.sex) +
+  scale_fill_manual(values = cnst$color.sex) +
   labs(
     x = NULL,
-    y = "Excess deaths"
-  )+
-  facet_wrap(~age.n.fct, ncol = 3)
+    y = 'Cumulative excess deaths'
+  ) +
+  geom_text(
+    aes(x = x, y = y, label = label),
+    data =
+      data.frame(
+        age.n = '85+', x = as.Date('2020-07-01'), y = 13.5e3,
+        label = 'Female'
+      ),
+    color = cnst$color.sex['f']
+  ) +
+  geom_text(
+    aes(x = x, y = y, label = label),
+    data =
+      data.frame(
+        age.n = '85+', x = as.Date('2020-07-01'), y = 10e3,
+        label = 'Male'
+      ),
+    color = cnst$color.sex['m']
+  ) +
+  fig_spec$MyGGplotTheme(
+    hgrid = TRUE,
+    scaler = 1.3,
+    show_legend = FALSE
+  ) +
+  coord_cartesian(clip = 'off')
 
-Figure.2
-
-pdf(file = 'Figures/Figure_2.pdf',width = 9 ,height = 7)
-Figure.2
-dev.off()
-
-
-
+fig_spec$ExportPDF(
+  fig$figure2, filename = 'Figure_2', path = 'Figures',
+  width = fig_spec$width, height = 0.8*fig_spec$width
+)
 
